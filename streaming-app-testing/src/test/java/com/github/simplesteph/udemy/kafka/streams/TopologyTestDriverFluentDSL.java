9dfa82cf78d86b7f4ec0d075a733de7679ef9b45
DivLoic <ldivad@isep.fr>
2019-07-07 16:34:51 +0200

    add the mockedstreams and fluenttests exemaples

---
+++ streaming-app-testing/src/test/java/com/github/simplesteph/udemy/kafka/streams/TopologyTestDriverFluentDSL.java
@@ -0,0 +1,35 @@
+package com.github.simplesteph.udemy.kafka.streams;
+
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Properties;
+
+public class TopologyTestDriverFluentDSL {
+
+    Properties config;
+
+    StreamsBuilder builder;
+
+    @Before
+    public void setUpTopologyTestDriver(){
+        builder = new StreamsBuilder();
+        config = new Properties();
+
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+    }
+
+    @Test
+    public void characterCountOutputTest(){
+
+
+    }
+
+}
